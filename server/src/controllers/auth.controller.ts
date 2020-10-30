55f57c344b5e289f4c95ea8105b215cde216155b
MrOxotnik <mroxotnik.app@gmail.com>
2020-10-31 02:40:36 +0300

    Добавление в репозиторий задания

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,28 @@
+import passport from 'passport';
+import * as express from 'express';
+import { Request, Response, IRouter } from 'express'
+import IControllerBase from './../interfaces/IControllerBase.interface'
+
+class AuthController implements IControllerBase {
+    public router = express.Router()
+
+    constructor() {
+        this.initRoutes()
+    }
+
+    public initRoutes(): IRouter {
+        this.router.get('/signin', this.signin)
+        this.router.get('/signup', this.signup)
+        return this.router;
+    }
+
+    signin = async (req: Request, res: Response) => {
+        res.json({ signin: 'work' });
+    }
+
+    signup = async (req: Request, res: Response) => {
+        res.json({ signup: 'work' });
+    }
+}
+
+export default AuthController
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,15 @@ import App from './App'
 import * as bodyParser from 'body-parser'
 import loggerMiddleware from './middleware/logger'
 
-import HomeController from "./controllers/home.controller";
+import HomeController from "./controllers/home.controller"
+import AuthController from "./controllers/auth.controller";
 
 
 const app = new App({
     port: 5000,
     controllers: [
         new HomeController(),
+        new AuthController(),
     ],
     middleWares: [
         bodyParser.json(),
